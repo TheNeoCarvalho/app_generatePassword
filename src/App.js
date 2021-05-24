c67b72db91cfaa5ce614ed728628c0ed29ddc9da
Manoel Carvalho <manoel.carvalho@mediacaonline.com>
2021-05-24 09:12:10 -0300

    Gerando senha

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import {Text, View, Image, SafeAreaView, TextInput} from 'react-native';
-import {Container, Titulo, Lock, Input} from './styled';
+import React, {useState} from 'react';
+import {
+  Text,
+  View,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import {Container, Titulo, Lock, Input, Button, ButtonText} from './styled';
 const App = () => {
+  const [hash, setHash] = useState('');
+
+  function gen() {
+    const chars =
+      'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*-+';
+    const passwordLenght = hash;
+    let password = '';
+    for (let i = 0; i < passwordLenght; i++) {
+      let generated = Math.floor(Math.random() * chars.length);
+      password += chars.substring(generated, generated + 1);
+    }
+    setHash(password);
+  }
+
   return (
-    <SafeAreaView>
-      <Container>
-        <Titulo>Gerador de Senhas</Titulo>
-        <Lock
-          source={{
-            uri: 'https://static.thenounproject.com/png/3309848-200.png',
-          }}
-        />
-        <Input placeholder="useless placeholder" keyboardType="numeric" />
-      </Container>
-    </SafeAreaView>
+    <Container>
+      <Titulo>Gerador de Senhas</Titulo>
+      <Lock
+        source={{
+          uri: 'https://static.thenounproject.com/png/3309848-200.png',
+        }}
+      />
+      <Input
+        placeholder="Tamanho da senha a ser gerada"
+        keyboardType="numeric"
+        onChangeText={e => setHash(e)}
+        value={hash}
+      />
+      <Button onPress={gen}>
+        <ButtonText>Gerar Senha</ButtonText>
+      </Button>
+    </Container>
   );
 };
 
--- src/styled.js
+++ src/styled.js
@@ -4,6 +4,7 @@ export const Container = styled.View`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  flex: 1;
 `;
 
 export const Titulo = styled.Text`
@@ -17,11 +18,26 @@ export const Lock = styled.Image`
   margin-top: 10px;
 `;
 export const Input = styled.TextInput`
+  font-size: 18px;
   width: 90%;
   height: 50px;
   padding: 2px 10px;
   margin-top: 10px;
   border: 1px solid #ccc;
   border-radius: 50px;
+  text-align: center;
+`;
+
+export const Button = styled.TouchableOpacity`
+  background-color: #222;
+  margin-top: 10px;
+  padding: 10px;
+  width: 90%;
+  border-radius: 50px;
+`;
+
+export const ButtonText = styled.Text`
+  font-size: 22px;
+  color: #fff;
   text-align: center;
 `;
